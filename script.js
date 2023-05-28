0ba01a4e68439d327177de3b53e5630a676096ea
datochakhunashvili <130147418+datochakhunashvili@users.noreply.github.com>
2023-05-28 20:45:30 +0400

    add.js for index

---
+++ script.js
@@ -0,0 +1,5 @@
+let numbersArray =[1,2,3,4,5,6,7,8,9,10,20,30,35,55,60,80,95,100];
+console.log(numbersArray.length);
+for (let index = 0; index < numbersArray. length; index++) {
+    console.log(numbersArray[index]);
+}
