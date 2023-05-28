a9899295f0662fc08e4373c9d31a9b3c3091a526
datochakhunashvili <130147418+datochakhunashvili@users.noreply.github.com>
2023-05-28 21:06:07 +0400

    add. js,  push, pop,shift,unshift

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ let numbersArray =[1,2,3,4,5,6,7,8,9,10,20,30,35,55,60,80,95,100];
 console.log(numbersArray.length);
 for (let index = 0; index < numbersArray. length; index++) {
     console.log(numbersArray[index]);
-}
+};
+
+let names = ["dato","giorgi","luka","sandro","andria"];
+console.log(names);
+names.push("nikolozi");
+console.log(names);
+names.pop();
+console.log(names);
+names.shift();
+console.log(names);
+names.unshift("beqa");
+console.log(names);
