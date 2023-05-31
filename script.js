baca9fc4a0d5c6b53d187e88dfd1537e215286a3
datochakhunashvili <130147418+datochakhunashvili@users.noreply.github.com>
2023-05-31 13:24:44 +0400

    add. 1,2 js

--- script.js
+++ script.js
@@ -1,47 +1,59 @@
-let numbersArray =[1,2,3,4,5,6,7,8,9,10,20,30,35,55,60,80,95,100];
-console.log(numbersArray.length);
-for (let index = 0; index < numbersArray. length; index++) {
-    console.log(numbersArray[index]);
-};
+// let numbersArray =[1,2,3,4,5,6,7,8,9,10,20,30,35,55,60,80,95,100];
+// console.log(numbersArray.length);
+// for (let index = 0; index < numbersArray. length; index++) {
+//     console.log(numbersArray[index]);
+// };
 
-let names = ["dato","giorgi","luka","sandro","andria"];
-console.log(names);
-names.push("nikolozi");
-console.log(names);
-names.pop();
-console.log(names);
-names.shift();
-console.log(names);
-names.unshift("beqa");
-console.log(names);
+// let names = ["dato","giorgi","luka","sandro","andria"];
+// console.log(names);
+// names.push("nikolozi");
+// console.log(names);
+// names.pop();
+// console.log(names);
+// names.shift();
+// console.log(names);
+// names.unshift("beqa");
+// console.log(names);
 
-let newNumbersArray = [1,2,3,4,5];
-for (let index = 0; index < newNumbersArray. length; index++) {
-    console.log(newNumbersArray [index] + 10);
-    console.log(newNumbersArray [index] *7)
+// let newNumbersArray = [1,2,3,4,5];
+// for (let index = 0; index < newNumbersArray. length; index++) {
+//     console.log(newNumbersArray [index] + 10);
+//     console.log(newNumbersArray [index] *7)
+// }
+
+// let numbersAr =[1,2,3,4,5,6,7,8,9,10,20,30,35,55,60,80,95,100];
+// console.log(numbersAr.length);
+// for (let index = 0; index < numbersAr. length; index++) {
+//     console.log(numbersAr[index]);
+//     if (Number % 2 === 0) {
+//         numbersAr = true;
+//         console.log(numbersAr);
+//     } else {
+//         numbersAr = "Even";
+//         console. log(numbersAr)
+//     }
+// }
+
+// let texts = ["MY NAME IS JOHN"];
+// for (let index = 0; index < texts. length; index++) { 
+//     console. log(texts[index])
+//     if (texts.length <3) {
+//         let   upperCaseTexts = texts.toUpperCase();
+//         console. log(upperCaseTexts);
+//     }else {
+//         let lowerCaseTexts = texts.toLowerCase(); 
+//         console.log(lowerCaseTexts);
+//     }
+// }
+
+let numbers = [];
+for (let i = 0; i <= 100; i++) {
+    console. log (i);
 }
 
-let numbersAr =[1,2,3,4,5,6,7,8,9,10,20,30,35,55,60,80,95,100];
-console.log(numbersAr.length);
-for (let index = 0; index < numbersAr. length; index++) {
-    console.log(numbersAr[index]);
-    if (Number % 2 === 0) {
-        numbersAr = true;
-        console.log(numbersAr);
-    } else {
-        numbersAr = "Even";
-        console. log(numbersAr)
-    }
+let index = 0;
+while (index < 50) {
+    console.log(index);
+    index++;
 }
 
-let texts = ["MY NAME IS JOHN"];
-for (let index = 0; index < texts. length; index++) { 
-    console. log(texts[index])
-    if (texts.length <3) {
-        let   upperCaseTexts = texts.toUpperCase();
-        console. log(upperCaseTexts);
-    }else {
-        let lowerCaseTexts = texts.toLowerCase(); 
-        console.log(lowerCaseTexts);
-    }
-}
